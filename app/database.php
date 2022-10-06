15b7e743d6e1e77f7713f37910a1b93332c35fdb
bs4p <bambang.s4p@gmail.com>
2022-10-06 11:31:01 +0700

    remove type hinting for Database.php (php7)

--- app/database.php
+++ app/database.php
@@ -2,19 +2,19 @@
 
 class Database
 {
-  private string $dbHost = '127.0.0.1';
-  private string $dbUser = 'root';
-  private string $dbPass = '';
-  private string $dbName = 'web0';
-  private int $port = 3306;
-  private array $dbOption = [
+  private $dbHost = '127.0.0.1';
+  private $dbUser = 'root';
+  private $dbPass = '';
+  private $dbName = 'web0';
+  private $port = 3306;
+  private $dbOption = [
     PDO::ATTR_PERSISTENT => true,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
   ];
 
-  private PDO $dbh;
+  private $dbh;
 
-  private PDOStatement $stmt;
+  private $stmt;
 
   public function __construct()
   {
@@ -26,19 +26,19 @@ class Database
     }
   }
 
-  public function query(string $query)
+  public function query($query)
   {
     $this->dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $this->dbh->query($query);
   }
 
-  public function prepare(string $query)
+  public function prepare($query)
   {
     $this->stmt = $this->dbh->prepare($query);
     return $this;
   }
 
-  public function bindValue(mixed $param, mixed $var, ?int $type = null)
+  public function bindValue($param, $var, $type = null)
   {
     if (is_null($type)) {
       switch (true) {
@@ -60,7 +60,7 @@ class Database
     return $this;
   }
 
-  public function execute(?array $params = null)
+  public function execute($params = null)
   {
     if (is_null($params)) {
       $this->stmt->execute();
@@ -70,7 +70,7 @@ class Database
     return $this;
   }
 
-  public function fetch(?array $executeParams = null)
+  public function fetch($executeParams = null)
   {
     $this->execute($executeParams);
     $result = $this->stmt->fetch(PDO::FETCH_NUM);
@@ -78,7 +78,7 @@ class Database
     return ($result == false) ? false : $result[0];
   }
 
-  public function fetchAll(?array $executeParams = null)
+  public function fetchAll($executeParams = null)
   {
     $this->execute($executeParams);
     $result = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -91,7 +91,7 @@ class Database
     return $this->stmt->rowCount();
   }
 
-  public function quote(string $string)
+  public function quote($string)
   {
     return $this->dbh->quote($string);
   }
